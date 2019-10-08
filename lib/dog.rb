8ec3466d291dde4aa095995d885fa387d6184fce
Jesse Smith <jesse.lee.smith@gmail.com>
2019-10-08 21:49:44 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Dog
+  def name=(new_name)
+    @this_dogs_name = new_name
+  end
+
+  def name
+    @this_dogs_name
+  end
+
+  def bark
+    puts "woof!"
+  end
+end
