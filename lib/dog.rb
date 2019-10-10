8ec260af4ac0001b60f02419a4121dbdd51b06fe
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-10 13:56:47 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+
+class Dog 
+  attr_accessor :name
+  
+  #The above accessor is the same as this:
+  # def name=(dog_name)
+  #   @name=dog_name
+  # end
+  
+  # def name
+  #   @name
+  # end
+  
+  def bark
+    puts "woof!"
+  end
+  
+end
