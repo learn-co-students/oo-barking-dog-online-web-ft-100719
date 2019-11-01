c2003f00650512193814a238e9cd57556d72865e
Duong Dang <dddangmedia@gmail.com>
2019-11-01 19:57:27 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Dog 
+  def name=(dog_name)
+    @given_dog_name = dog_name 
+  end
+  
+  def name
+    @given_dog_name
+  end
+  
+  def bark 
+    puts "woof!"
+  end
+end
+
+fido = Dog.new 
+fido.name = "Fido"
